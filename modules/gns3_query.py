6c1a5d8c4efd0b14a96af4dbd2a4cc2b53a29f0b
Evan Rogers <evan.rogers@broadcom.com>
2023-05-22 15:23:23 -0500

    Update

--- modules/gns3_query.py
+++ modules/gns3_query.py
@@ -145,7 +145,7 @@ def get_node_links(nodes, links, server, port, project_id, node_id, node_name, r
         for node in link["nodes"]:
             node_labels.append(node["label"]["text"])
         link_id = link['link_id']
-        print(link_id)
+        print(node_labels)
         link_url = f"http://{server}:{port}/v2/projects/{project_id}/links/{link_id}"
         response = requests.get(link_url)
         link_data = response.json()
