c3a43d68084b052ebdf73c1ea1b321573f9988fa
Evan Rogers <evan.rogers@broadcom.com>
2023-06-23 09:57:12 -0500

    Update

--- app.py
+++ app.py
@@ -208,7 +208,7 @@ def viptela_deploy_full():
     return jsonify({'success': True})
 
 @app.route('/api/tasks/start_viptela_appneta_deploy', methods=['PUT'])
-def viptela_deploy_full():
+def viptela_appneta_deploy_full():
     global running_thread
     # Check if a thread is already running
     if running_thread is not None and running_thread.is_alive():
