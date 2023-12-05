0832d7c1a9f6ddce68de4843268896381ce91d90
Evan Rogers <evan.rogers@broadcom.com>
2023-12-05 13:31:33 -0600

    Update

--- modules/use_case/use_cases.py
+++ modules/use_case/use_cases.py
@@ -142,7 +142,7 @@ def use_case_2(server, port, project_id, state):
     return {'message': 'Scenario started successfully.'}, 200
 
 def use_case_3(server, port, project_id, state):
-    remote_node_name = 'SanFran-Client'
+    remote_node_name = 'SanFran-Client-2'
     nodes = gns3_query_get_nodes(server, port, project_id)
     remote_node_id, remote_node_console, remote_node_aux = gns3_query_find_node_by_name(nodes, remote_node_name)
     client_command = f'python3 /home/torrent_use_case.py 6681'
