648f466f794407d37381ba1a068591c715b861a9
Evan Rogers <evan.rogers@broadcom.com>
2024-01-10 13:19:35 -0700

    Update

--- utilities/project_startup/project_start.py
+++ utilities/project_startup/project_start.py
@@ -18,7 +18,7 @@ def main():
         "python3 start_nodes_utility.py 192.168.122.1 80 multivendor-sdwan start bgp,isp,Versa,FlexVNF,sw-core,sw-dist,sw-acc,Client,Server,Tix",
         "python3 latency_utility.py 192.168.122.1 80 multivendor-sdwan add bgp",
         "python3 latency_utility.py 192.168.122.1 80 multivendor-sdwan add isp-houston",
-        "python3 bandwidth_utility.py 192.168.122.1 80 multivendor-sdwan add isp-newyork,isp-seattle,isp-sanfran,isp-atlanta",
+        "python3 bandwidth_utility.py 192.168.122.1 80 multivendor-sdwan add isp-newyork,isp-seattle,isp-sanfran,isp-atlanta,isp-miami",
         "python3 start_nodes_utility.py 192.168.122.1 80 multivendor-sdwan start ISP_Router",
         "python3 isp_route_utility.py 192.168.122.1 80 multivendor-sdwan ISP_Router",
         "python3 start_nodes_utility.py 192.168.122.1 80 multivendor-sdwan start vk35,vManage,vBond,vSmart,vEdge",
